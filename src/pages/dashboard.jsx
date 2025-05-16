a8ef3594ad12383170127b39f3cc0414985034d7
Felipe Araújo <felipearaujobs@hotmail.com>
2025-05-15 22:44:42 -0300

    endpoints change

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -29,8 +29,8 @@ export default function Dashboard() {
   const fetchTasks = async () => {
     try {
       const res = await axios.get(
-        //"https://task-manager-api-zmo4.onrender.com/task/find",
-        "http://localhost:5000/task/find",
+        "https://task-manager-api-zmo4.onrender.com/task/find",
+        //"http://localhost:5000/task/find",
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -58,8 +58,8 @@ export default function Dashboard() {
 
     try {
       const res = await axios.post(
-        //"https://task-manager-api-zmo4.onrender.com/task/register",
-        "http://localhost:5000/task/register",
+        "https://task-manager-api-zmo4.onrender.com/task/register",
+        //"http://localhost:5000/task/register",
         {
           title,
           description,
@@ -93,8 +93,8 @@ export default function Dashboard() {
   const handleDelete = async (id) => {
     try {
       await axios.delete(
-        //`https://task-manager-api-zmo4.onrender.com/task/delete/${id}`,
-        `http://localhost:5000/task/delete/${id}`,
+        `https://task-manager-api-zmo4.onrender.com/task/delete/${id}`,
+        //`http://localhost:5000/task/delete/${id}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -110,8 +110,8 @@ export default function Dashboard() {
   const handleEdit = async (id, updatedData) => {
     try {
       await axios.put(
-        //`https://task-manager-api-zmo4.onrender.com/task/update/${id}`,
-        `http://localhost:5000/task/update/${id}`,
+        `https://task-manager-api-zmo4.onrender.com/task/update/${id}`,
+        //`http://localhost:5000/task/update/${id}`,
         updatedData,
         {
           headers: {
